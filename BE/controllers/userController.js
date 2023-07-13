234bab1a9f7d5422e0dc7ce214c18117e2fb8ef9
AlexDragoi7 <dragoialexandru26@gmail.com>
2023-07-13 15:09:01 +0300

    Added login/reset pw/userAuth middleware etc.

--- BE/controllers/userController.js
+++ BE/controllers/userController.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const schema = require("../models/userModel");
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+
 
 const User = mongoose.model('User', schema);
 
@@ -31,6 +32,78 @@ const createUser = async (req, res) => {
     }
 }
 
+async function loginUser (req, res) {
+    try{
+
+        var {email, password} = req.body
+
+        var user = await User.findOne({
+            email: email
+        })
+
+        await user.save()
+
+        if (user){
+            var isSame = await bcrypt.compare(password, user.password)
+            if (isSame){
+                let token = jwt.sign({id: user.id}, process.env.SECRETKEY, {expiresIn: "2d"})
+            
+
+            res.cookie("token", token,{
+                maxAge: 2 * 24 * 60 * 60,
+                httpOnly: true, 
+            })
+            console.log("user", JSON.stringify(user, null, 2))
+            console.log(token)
+
+            return res.status(201).send({
+                id: user.id,
+                first_name: user.first_name,
+                email: user.email,
+                accessToken: token,
+            })
+        }else{
+            return res.status(401).send("Authentication failed")
+        }
+         
+        }else{
+            return res.status(401).send("Authentication failed")
+        }
+
+    }catch(err){
+        console.error(err)
+    }
+}
+
+
+async function resetPassword (req, res, next){
+    try{
+
+        var {new_password, email} = req.body
+
+        var data = new User({
+            password: await bcrypt.hash(new_password, 10)
+        })
+
+        var updatedPass = await User.updateOne({
+            email: email
+        }, {
+            password: data.password
+        })
+
+        if(updatedPass[0] != 0){
+            return res.status(200).json({
+                message: "Password has been successfully updated"
+            })
+        }else{
+            return res.status(401).send(Error)
+        }
+
+    }catch(err){
+        console.error(err)
+    }
+}
+
 const getAllUsers = async (req, res) => {
     try{
         
@@ -44,5 +117,5 @@ const getAllUsers = async (req, res) => {
 
 
 
-module.exports = {createUser, getAllUsers};
+module.exports = {createUser, getAllUsers, loginUser, resetPassword};
 
--- BE/routes/userRoute.js
+++ BE/routes/userRoute.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const userController = require('../controllers/userController')
-const {createUser, getAllUsers} = userController;
+const {createUser, getAllUsers, loginUser, resetPassword} = userController;
+var userAuth = require("../middleware/userAuth")
 
 
 const router = express.Router();
 
-router.post("/",createUser);
+router.post("/signup",userAuth.saveUser, createUser);
 router.get("/", getAllUsers)
+router.patch("/resetpassword", resetPassword)
+
+router.post("/login", loginUser)
+
 
 module.exports = router;
